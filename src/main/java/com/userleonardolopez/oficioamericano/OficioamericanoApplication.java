6de512ef22bd553a771ef9f8b9783cc9ec747681
userleonardolopez <xleo@outlook.es>
2025-05-18 18:44:57 -0700

    Initial files from Spring Starter and N-Tier packages introduced

---
+++ src/main/java/com/userleonardolopez/oficioamericano/OficioamericanoApplication.java
@@ -0,0 +1,13 @@
+package com.userleonardolopez.oficioamericano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficioamericanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficioamericanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userleonardolopez/oficioamericano/OficioamericanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userleonardolopez.oficioamericano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficioamericanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
